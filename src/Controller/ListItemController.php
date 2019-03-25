08e83ea5b6980decabe0c5e62ccceb01ad48affa
= <=>
2019-03-25 16:35:37 +0200

    added notFoundHttpException component

--- src/Controller/ListItemController.php
+++ src/Controller/ListItemController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 //use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use App\Entity\ListItem;
 
--- src/Controller/ListItemDescriptionController.php
+++ src/Controller/ListItemDescriptionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use App\Entity\ListItem;
 
